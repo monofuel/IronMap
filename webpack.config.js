0f777003130f27523ba3208ec1be8359fc4ed5e9
Andrew Brower <monofuel@japura.net>
2017-04-13 18:00:03 -0700

    tweak react

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+
+const webpack = require('webpack');
 module.exports = {
     entry: "./src/test.tsx",
     output: {
@@ -23,13 +25,9 @@ module.exports = {
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
     },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            "React": "react",
+        }),
+    ],
 };
